14d3c27023174b0f572d0f3cc6926f725aa77006
NathanEid <nesoeid4@gmail.com>
2023-01-24 21:45:16 +0200

    creating script for vm to install google cloud cli and kubectl and the auth plugin to connect to the cluster

---
+++ vm/myscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update -y
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt install google-cloud-cli -y
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo chmod +x kubectl
+sudo mkdir -p ~/.local/bin
+sudo mv ./kubectl ~/.local/bin/kubectl
+
+sudo apt install google-cloud-sdk-gke-gcloud-auth-plugin
+echo "donedone" > done.txt
